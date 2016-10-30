7088f68903828ba27c2e1c082f4c0d8337e6f46c
liujinwene <278810263@qq.com>
2016-10-31 00:00:17 +0800

    news demo 2

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-// import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom';
+import NewsList from './js/NewsList.js';
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-        <div>Hello World</div>
-        );
-  }
-}
-
-ReactDom.render(<HelloWorld />, document.getElementById('app'));
-// render(<HelloWorld />, document.getElementById('app'));
+render(<NewsList />, document.getElementById('app'));
--- js/NewsHeader.js
+++ js/NewsHeader.js
@@ -1,3 +1,28 @@
 import React from 'react';
 
-class NewsHeader extends React.componet
+export default class NewsHeader extends React.Component {
+	render() {
+		return (
+			<div className ="newsHeader">
+				{this.getLogo()}
+				{this.getTitle()}
+			</div>
+			);
+	}
+
+	getLogo() {
+		return (
+		<div className="newsHeader-logo">
+			<a href="http://www.baidu.com"><img src="./images/1.png" /></a>
+		</div>
+		);	
+	}
+
+	getTitle() {
+		return (
+			<div className="newsHeader-title">
+				Hacker News
+			</div>
+		);
+	}
+}
